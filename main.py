9bf7193b21e903252b2d5af91a0d74c24a4ac33c
Randy Herritt <randy.herritt@gmail.com>
2025-08-12 14:04:35 -0300

    updates based on new api

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from minimal_agent_framework import Graph, Node, EventEmitter, tool
+from minimal_agent_framework import Graph, Node, EventEmitter, tool, context
 import logging
 from dotenv import load_dotenv
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 for name in ("httpx", "httpcore"):
     lg = logging.getLogger(name)
@@ -19,28 +19,28 @@ def get_magic_word() -> str:
 
 
 def create_nodes() -> list[Node]:
-
     test_node = (Node()
         .name('test')
-        .context_keys(["user_query"])
-        .input("Based on the user_query, provide some quick feedback to indicate how you plan to help.")
-        .routes([{
-            "research": "the query requires research in order to answer",
-            "code": "the query is about writing code"
-        }])
+        .input("Based on the user_query, provide some quick feedback to indicate how you plan to help. Tell them you can help.")
+
     )
-    
     research_node = (Node()
         .name("research")
-        .context_keys(["user_query"])
         .input("Tell the user that you have started reseaching to answer their query")
     )
-
     code_node = (Node()
         .name("code")
-        .context_keys(["user_query"])
         .input("Tell the user that you will write some code to answer their query")
     )
+
+    # define routes
+    test_node.routes({
+        research_node._id: "you need to do research",
+        code_node._id: "the query relates to code"
+    })
+
+
+
 
     return [test_node, research_node, code_node]
 
@@ -49,13 +49,14 @@ if __name__ == "__main__":
     events = EventEmitter()
     events.on("text", handler)
 
-    context = {
-        "user_query": "write a program in python to add 2 numbers together"
-    }
+    context.events = events
 
-    graph = Graph(events)
+    query = input("User: ")
+    context.user_query = query
+
+    graph = Graph()
     nodes = create_nodes()
     graph.add_nodes(nodes)
 
-    graph.run(nodes[0], context)
+    graph.run(nodes[0])
 
